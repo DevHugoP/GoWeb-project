db37dae539de4ca87ee3f7a4117ecc2a0abc2ee6
hugo polchetti <h.polchetti@gmail.com>
2021-05-04 12:20:53 +0200

    code de la partie tableau du site

--- javascript/home.js
+++ javascript/home.js
@@ -1,20 +1,57 @@
-fetch("https://fakestoreapi.com/products")
+window.onhashchange = localStorage.clear();
+
+fetch("https://fakestoreapi.com/products?limit=7")
 	.then((res) => res.json())
 	.then((fetchedData) => homepageData(fetchedData));
 
 const homepageData = (fetchedData) => {
 	for (let data of fetchedData) {
 		//on fait une boucle dans le JSON récupéré pour sortir chacun des éléments
-		console.log(data);
 
 		//recupération de la div qui contiendra le contenu
-		const tableContainer = document.getElementById("tablecontainer");
-
+		let tbody = document.getElementById("tbody");
 		//Création des éléments du DOM nécessaires
-		const blocArticle = document.createElement("tr");
-		const blocCamera = document.createElement("td");
-		const imageBoite = document.createElement("td");
-		const titreCamera = document.createElement("td");
-		const prixCamera = document.createElement("td");
+		let productLine = document.createElement("tr");
+		let productName = document.createElement("td");
+		productName.className = "productName";
+		let productCategoryContainer = document.createElement("td");
+		let productCategory = document.createElement("p");
+		let productPrice = document.createElement("td");
+		let productPriceVat = document.createElement("td");
+		productPriceVat.className = "productPriceVat";
+
+		// Ajout des classes et inserer les data
+		productLine.className = "rowContainer";
+		productLine.setAttribute(
+			"data-href",
+			"http://127.0.0.1:5500/pages/product.html?id=" + data.id
+		);
+		productName.textContent = data.title;
+		productCategory.textContent = data.category;
+
+		// On ajoute une classe en fonction de la category
+		if (productCategory.textContent === "men's clothing") {
+			productCategory.className = "orangeTag";
+		} else {
+			productCategory.className = "greenTag";
+		}
+
+		productPrice.textContent = data.price + "€";
+		let modifiedPrice = data.price + (data.price / 100) * 20;
+		productPriceVat.textContent = modifiedPrice.toFixed(2) + "€";
+
+		//insertion des données dans les balises
+		productCategoryContainer.append(productCategory);
+		productLine.append(productName, productCategoryContainer, productPrice, productPriceVat);
+		tbody.append(productLine);
 	}
+
+	const rows = document.querySelectorAll("tr[data-href]");
+	console.log(rows);
+
+	rows.forEach((row) => {
+		row.addEventListener("click", () => {
+			window.location.href = row.dataset.href;
+		});
+	});
 };
--- javascript/product.js
+++ javascript/product.js
@@ -1,83 +1,90 @@
+window.onhashchange = localStorage.clear();
+
+let urlProduct = window.location.href; // recupération de l'url contenant notre id de produit
+const splitUrl = urlProduct.split("=");
+idProduct = splitUrl[1];
+
 // fonction permettant d'attendre le chargement du DOM
 document.addEventListener("DOMContentLoaded", () => {
 	//récupération des données de l'API
-	fetch("https://fakestoreapi.com/products/1")
+	fetch("https://fakestoreapi.com/products/" + idProduct)
 		.then((resp) => resp.json())
 		.then((product) => productData(product));
 
 	function productData(product) {
 		localStorage.setItem("product", JSON.stringify(product));
-	}
-});
 
-let parsedProduct = JSON.parse(localStorage.getItem("product"));
-//remplissage du titre en récupérant la balise de titre
-document.getElementById("productTitle").textContent = parsedProduct.title;
+		let parsedProduct = JSON.parse(localStorage.getItem("product"));
+		//remplissage du titre en récupérant la balise de titre
+		document.getElementById("productTitle").textContent = parsedProduct.title;
 
-//CREATION DE LA BALISE img
-//récupération de la balise avec son ID
-let productImg = document.getElementById("productImg");
-//création d'une balise img avec sa classe et son attribut
-let img = document.createElement("img");
-img.setAttribute("src", parsedProduct.image);
-img.setAttribute("alt", "image d'un produit du site voir description");
-img.classList.add("imgContainer__productImg");
-//injection de la balise avec le contenu dans le DOM
-productImg.append(img);
+		//CREATION DE LA BALISE img
+		//récupération de la balise avec son ID
+		let productImg = document.getElementById("productImg");
+		//création d'une balise img avec sa classe et son attribut
+		let img = document.createElement("img");
+		img.setAttribute("src", parsedProduct.image);
+		img.setAttribute("alt", "image d'un produit du site voir description");
+		img.classList.add("imgContainer__productImg");
+		//injection de la balise avec le contenu dans le DOM
+		productImg.append(img);
 
-//balise p de descrition du produit
-document.getElementById("descriptionText").textContent = parsedProduct.description;
+		//balise p de descrition du produit
+		document.getElementById("descriptionText").textContent = parsedProduct.description;
 
-//BALISE p CATEGORY.
-// si la category est men's product alors la couleur de la pastille sera orange sinon bleu
+		//BALISE p CATEGORY.
+		// si la category est men's product alors la couleur de la pastille sera orange sinon bleu
 
-document.getElementById("categoryText").textContent = parsedProduct.category;
-if (parsedProduct.category === "men's clothing") {
-	document.getElementById("categoryText").style.backgroundColor = "#ffa846";
-} else {
-	document.getElementById("categoryText").style.backgroundColor = "#32b436";
-}
+		document.getElementById("categoryText").textContent = parsedProduct.category;
+		if (parsedProduct.category === "men's clothing") {
+			document.getElementById("categoryText").style.backgroundColor = "#ffa846";
+		} else {
+			document.getElementById("categoryText").style.backgroundColor = "#32b436";
+		}
 
-//Balise input Prix du produit
-// on calcule le prix de la TVA du produit avant modification
-let price = parseFloat((document.getElementById("priceInput").value = parsedProduct.price));
-let addVat = price + (price / 100) * 20;
-document.getElementById("priceVat").textContent = "Price (including VAT) : " + addVat + " €";
+		//Balise input Prix du produit
+		// on calcule le prix de la TVA du produit avant modification
+		let price = parseFloat((document.getElementById("priceInput").value = parsedProduct.price));
+		let addVat = price + (price / 100) * 20;
+		document.getElementById("priceVat").textContent =
+			"Price (including VAT) : " + addVat + " €";
 
-// création d'une variable qui sera modifiée par l'utilisateur
-let newPrice = parseFloat(document.getElementById("priceInput").value);
-console.log(newPrice);
+		// création d'une variable qui sera modifiée par l'utilisateur
+		let newPrice = parseFloat(document.getElementById("priceInput").value);
+		console.log(newPrice);
 
-// On recalcule le prix avec la TVA à chaque input de l'utilisateur
-document.getElementById("priceInput").addEventListener("keyup", () => {
-	// conversion de l'input de string à number
-	newPrice = parseFloat(document.getElementById("priceInput").value);
+		// On recalcule le prix avec la TVA à chaque input de l'utilisateur
+		document.getElementById("priceInput").addEventListener("keyup", () => {
+			// conversion de l'input de string à number
+			newPrice = parseFloat(document.getElementById("priceInput").value);
 
-	// si le user input est vide on remplace le NaN par 0
-	if (isNaN(newPrice)) {
-		newPrice = 0;
-	}
+			// si le user input est vide on remplace le NaN par 0
+			if (isNaN(newPrice)) {
+				newPrice = 0;
+			}
 
-	// ajout du nouveau prix recalculé dynamiquement dand la balise appropriée
-	let newVatPrice = newPrice + (newPrice / 100) * 20;
-	document.getElementById("priceVat").textContent =
-		"Price (including VAT) : " + newVatPrice.toFixed(2) + " €";
-});
+			// ajout du nouveau prix recalculé dynamiquement dand la balise appropriée
+			let newVatPrice = newPrice + (newPrice / 100) * 20;
+			document.getElementById("priceVat").textContent =
+				"Price (including VAT) : " + newVatPrice.toFixed(2) + " €";
+		});
 
-document.getElementById("uploadBtn").addEventListener("click", () => {
-	// ici on vérifie que le prix à bien été modifié par le user, qu'il est différent du prix de base et de 0 et si son Datatype = number .
-	if (!isNaN(newPrice) && newPrice != price && newPrice != 0) {
-		// Envoi de la modification du prix si celui si a été modifié (les modifications ne sont pas réelement  enregistrées dans la BDD )
-		fetch("https://fakestoreapi.com/products/1", {
-			method: "PUT",
-			body: JSON.stringify({
-				price: newPrice
-			})
-		})
-			.then((res) => res.json())
-			.then((json) => console.log(json));
-		alert(" Prix modifié ");
-	} else {
-		alert("vous devez d'abord modifier le prix");
+		document.getElementById("uploadBtn").addEventListener("click", () => {
+			// ici on vérifie que le prix à bien été modifié par le user, qu'il est différent du prix de base et de 0 et si son Datatype = number .
+			if (!isNaN(newPrice) && newPrice != price && newPrice != 0) {
+				// Envoi de la modification du prix si celui si a été modifié (les modifications ne sont pas réelement  enregistrées dans la BDD )
+				fetch("https://fakestoreapi.com/products/1", {
+					method: "PUT",
+					body: JSON.stringify({
+						price: newPrice
+					})
+				})
+					.then((res) => res.json())
+					.then((json) => console.log(json));
+				alert(" Prix modifié ");
+			} else {
+				alert("vous devez d'abord modifier le prix");
+			}
+		});
 	}
 });
